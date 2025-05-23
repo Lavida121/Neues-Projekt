112ef6c8dcd1edf139f8ffd905417eea6a217a7c
Lavida121 <lavidaloca2312@gmail.com>
2025-05-23 02:16:56 +0200

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,23 +1,46 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import type { MapContainerProps } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-export default function Map({ stations }: { stations: any[] }) {
-  const center = stations.length ? [stations[0].lat, stations[0].lng] : [48.1351, 11.5820];
+type Props = {
+  stations: any[];
+};
+
+export default function Map({ stations }: Props) {
+  const center: [number, number] = stations.length
+    ? [stations[0].lat, stations[0].lng]
+    : [48.1351, 11.5820];
 
   return (
-    <MapContainer center={center} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer
+      center={center}
+      zoom={13}
+      style={{ height: '400px', width: '100%' }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
       {stations.map((s) => (
-        <Marker key={s.id} position={[s.lat, s.lng]} icon={L.icon({ iconUrl: 'https://cdn-icons-png.flaticon.com/512/252/252025.png', iconSize: [25, 41], iconAnchor: [12, 41] })}>
+        <Marker
+          key={s.id}
+          position={[s.lat, s.lng]}
+          icon={L.icon({
+            iconUrl: 'https://cdn-icons-png.flaticon.com/512/252/252025.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+          })}
+        >
           <Popup>
-            <strong>{s.name}</strong><br/>
-            {s.street}, {s.place}<br/>
-            Diesel: {s.diesel} €<br/>
-            E5: {s.e5} €<br/>
+            <strong>{s.name}</strong>
+            <br />
+            {s.street}, {s.place}
+            <br />
+            Diesel: {s.diesel} €
+            <br />
+            E5: {s.e5} €
+            <br />
             E10: {s.e10} €
           </Popup>
         </Marker>
